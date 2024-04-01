ba868059beb1588ab66292238a548030f7fb46b5
Veasy <zveasy327@gmail.com>
2024-03-31 20:19:59 -0500

    update:

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
+import SignUp from "./Signup";
 
 
 const AppRoutes = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<Login />} />
+      {/* <Route path="/" element={<Login />} /> */}
+      <Route path="/" element={<SignUp />} />
     </Routes>
   </Router>
 );
