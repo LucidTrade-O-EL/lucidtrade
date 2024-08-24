493d8e969fecad7369c67b28722445003a70747a
Veasy <zveasy327@gmail.com>
2024-08-24 14:36:57 -0500

    maps update

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import AppRoutes from "./Routes"; // Note the change here to match the new component name
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
--- src/common-components/GoogleEarth/GoogleEarth.tsx
+++ src/common-components/GoogleEarth/GoogleEarth.tsx
@@ -1,7 +1,32 @@
 import "./GoogleEarth.css";
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+} from "@vis.gl/react-google-maps";
 
 const GoogleEarth = () => {
-  return <div className="GoogleEarth"></div>;
+  return (
+    <div className="GoogleEarth">
+      <APIProvider
+        apiKey={"AIzaSyACOSlepxS0umdp8HaDICPxXYzVX2jDick"}
+        onLoad={() => console.log("Maps API has loaded.")}
+      >
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom
+            )
+          }
+        ></Map>
+      </APIProvider>
+    </div>
+  );
 };
 
 export default GoogleEarth;
